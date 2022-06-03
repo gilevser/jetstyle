f82f0c6d716b38329a4f2b28f147ecc423151a9b
Sergey <sincer89@mail.ru>
2022-06-03 10:46:14 +0500

    add h1 No empty

--- jet-style-test/src/components/BookList.jsx
+++ jet-style-test/src/components/BookList.jsx
@@ -6,12 +6,14 @@ const BookList = () => {
     const { books } = useContext(BooksContext)
 
     return (
-        <div className='mt-2'>
-            {books.map(book =>
-            <BookListItem book={book} key={book.id}
-            />
-            )}
-        </div>
+        books.length ?
+            <div className='mt-2'>
+                {books.map(book =>
+                    <BookListItem book={book} key={book.id}
+                    />
+                )}
+            </div> :
+            <h3 className="card-header text-center">Список пуст.</h3>
     );
 };
 
